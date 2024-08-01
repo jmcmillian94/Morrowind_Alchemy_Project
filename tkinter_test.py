971ee93e8669555e6af022fedf0dc577aaed3294
unknown <jmcmillian94@aim.com>
2024-07-31 21:17:16 -0400

    updated

--- tkinter_test.py
+++ tkinter_test.py
@@ -4,26 +4,25 @@ import pygame_menu
 from pygame_menu import themes
  
 pygame.init()
-surface = pygame.display.set_mode((800, 600))
+surface = pygame.display.set_mode((600, 400))
  
 def set_difficulty(value, difficulty):
     print(value)
     print(difficulty)
  
-def start_the_game():
+def search_ingredient():
     pass
  
-def level_menu():
-    mainmenu._open(level)
+def search_effect():
+    mainmenu._open(effect)
  
  
-mainmenu = pygame_menu.Menu('Morrowind Alchemy Helper', 800, 600, theme=themes.THEME_SOLARIZED)
-mainmenu.add.text_input('Name: ', default='username', maxchar=20)
-mainmenu.add.button('Play', start_the_game)
-mainmenu.add.button('Levels', level_menu)
+mainmenu = pygame_menu.Menu('Morrowind Alchemy Helper', 600, 400, theme=themes.THEME_DARK)
+mainmenu.add.button('Search by Ingredient', search_ingredient)
+mainmenu.add.button('Search by Effect', search_effect)
 mainmenu.add.button('Quit', pygame_menu.events.EXIT)
  
-level = pygame_menu.Menu('Select a Difficulty', 600, 400, theme=themes.THEME_BLUE)
-level.add.selector('Difficulty :', [('Hard', 1), ('Easy', 2)], onchange=set_difficulty)
+effect = pygame_menu.Menu('choose an effect', 600, 400, theme=themes.THEME_DARK)
+effect.add.selector('Difficulty :', [('Hard', 1), ('Easy', 2)], onchange=set_difficulty)
  
 mainmenu.mainloop(surface)
