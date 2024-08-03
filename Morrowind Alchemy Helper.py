6ad8f7bf4dc9b4f04d76caa44f2859bf4d721f06
unknown <jmcmillian94@aim.com>
2024-08-03 19:22:05 -0400

    custom theme applied

--- Morrowind Alchemy Helper.py
+++ Morrowind Alchemy Helper.py
@@ -7,10 +7,20 @@ from ingredients_dict import listOfEffects
 
  
 pygame.init()
-surface = pygame.display.set_mode((1920, 1080))
+surface = pygame.display.set_mode((800,600))
 
-background_image = pygame.image.load('morrowind_background.png')
-background_image = pygame.transform.scale(background_image, (1920, 1080))
+background_image = pygame.image.load('morrowind_background_2.png')
+background_image = pygame.transform.scale(background_image, (800,600))
+
+custom_theme = themes.THEME_GREEN.copy()
+custom_theme.background_color = (0,0,0,0)
+custom_theme.title_font = pygame_menu.font.FONT_FIRACODE_BOLD_ITALIC
+custom_theme.widget_font = pygame_menu.font.FONT_FIRACODE_BOLD_ITALIC
+custom_theme.title_font_color = (0,0,0) 
+custom_theme.widget_font_color = (0,0,0)
+custom_theme.selection_color = (150,20,20)
+custom_theme.widget_selection_effect = pygame_menu.widgets.LeftArrowSelection(arrow_size=(10, 15), arrow_right_margin=5, arrow_vertical_offset=0, blink_ms=2)
+custom_theme.title_bar_style = pygame_menu.widgets.MENUBAR_STYLE_TITLE_ONLY
 
 def draw_background():
     surface.blit(background_image, (0, 0))
@@ -19,16 +29,6 @@ def ingredient_menu():
     mainmenu._open(ingredient_submenu)
     
 ingredient_result_label = None
-
-#def search_ingredient(input_text):
-#    global ingredient_result_label
-#    potion_ingredient = input_text.lower()
-#    if potion_ingredient in ingredients:
-#            effects = ingredients[potion_ingredient]
-#            ingredient_result_label.set_title(', '.join(ingredients[potion_ingredient]) if effects else 'No ingredients found.')
-#    else:
-#            ingredient_result_label.set_title('Effect not found. Please try again or type exit to return to previous menu.')   
- 
 
 def search_ingredient(selected, value):
     global ingredient_result_label
@@ -44,15 +44,6 @@ def effect_menu():
 
 effect_result_label = None
 
-#def search_effect(input_text):
-#    global effect_result_label
-#    potion_effect = input_text.lower()
-#    if potion_effect in listOfEffects:
-#        found_keys = [key for key, value in ingredients.items() if potion_effect in value]
-#        effect_result_label.set_title(', '.join(found_keys) if found_keys else 'No ingredients found.')
-#    else:
-#        effect_result_label.set_title('Effect not found. Please try again or type exit to return to previous menu.')    
-
 def search_effect(selected, value):
     global effect_result_label
     potion_effect = value.lower()
@@ -64,19 +55,16 @@ def search_effect(selected, value):
 
  
  
-mainmenu = pygame_menu.Menu('Morrowind Alchemy Helper', 1024, 768, theme=themes.THEME_DARK)
+mainmenu = pygame_menu.Menu('Morrowind Alchemy Helper', 800,600, theme=custom_theme)
 mainmenu.add.button('Search by Ingredient', ingredient_menu)
 mainmenu.add.button('Search by Effect', effect_menu)
 mainmenu.add.button('Quit', pygame_menu.events.EXIT)
 
-
- 
-effect_submenu = pygame_menu.Menu('Potion Effect Search', 1024, 768, theme=themes.THEME_DARK)
-#effect_submenu.add.text_input(title='effect: ', cursor_selection_enable= True, copy_paste_enable= True, onreturn=search_effect)
+effect_submenu = pygame_menu.Menu('Potion Effect Search', 800,600, theme=custom_theme)
 effect_submenu.add.dropselect(title='effect: ',items=[(effect, effect) for effect in listOfEffects],onchange=search_effect)
 effect_result_label = effect_submenu.add.label('Search results will appear here.', max_char=-1)
 
-ingredient_submenu = pygame_menu.Menu('Potion Ingredient Search', 1024, 768, theme=themes.THEME_DARK)
+ingredient_submenu = pygame_menu.Menu('Potion Ingredient Search', 800,600, theme=custom_theme)
 #(title='ingredient: ', cursor_selection_enable= True, copy_paste_enable= True, onreturn=search_ingredient)
 ingredient_submenu.add.dropselect(title='ingredient: ',items=[(ingredient, ingredient) for ingredient in ingredients.keys()],onchange=search_ingredient)
 ingredient_result_label = ingredient_submenu.add.label('Search results will appear here.', max_char=-1)
