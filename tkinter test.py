5a3f652e5df618952ace989c382790ec50247e53
unknown <jmcmillian94@aim.com>
2024-08-11 16:05:48 -0400

    update

--- tkinter test.py
+++ tkinter test.py
@@ -1,11 +1,32 @@
 from tkinter import *
 from ingredients_dict import *
+import pygame
 
 #making a root window
 root = Tk()
 root.title("Morrowind Alchemy Helper")
+icon = PhotoImage(file='alchemy.png')
+root.iconphoto(False, icon)
 root.geometry("600x400")
 root.configure(bg='#eab974')
+
+#initialize pygame mixer
+pygame.mixer.init()
+
+#fucntion that plays background music
+def play_background_music():
+    pygame.mixer.music.load("explore.wav")  # Load the music file
+    pygame.mixer.music.play(-1)  # Play the music in a loop (-1 means infinite loop)
+
+initial_volume = 0.2
+pygame.mixer.music.set_volume(initial_volume)
+
+#function that toggles music on/off
+def toggle_music():
+    if pygame.mixer.music.get_busy():  # Check if music is currently playing
+        pygame.mixer.music.pause()  # Pause the music
+    else:
+        pygame.mixer.music.unpause()  # Unpause the music
 
 # Create frames for layout
 left_frame = Frame(root)
@@ -57,7 +78,7 @@ def search_effect(value):
 # Function to display ingredient search
 def open_ingredient_search():
     clear_right_frame()
-    Label(right_frame, text="Choose an Ingredient:").grid(row=0, column=0, padx=10, pady=10)
+    Label(right_frame, text="Choose an Ingredient:").grid(row=0, column=0, padx=30, pady=10)
     ingredient_var = StringVar(right_frame)
     OptionMenu(right_frame, ingredient_var, *ingredients).grid(row=1, column=0, padx=10, pady=10)
     Button(right_frame, text="Search", command=lambda: ingredient_search(ingredient_var.get())).grid(row=2, column=0, padx=10, pady=10)
@@ -65,7 +86,7 @@ def open_ingredient_search():
 # Function to display effect search
 def open_effect_search():
     clear_right_frame()
-    Label(right_frame, text="Enter an Effect:").grid(row=0, column=0, padx=10, pady=10)
+    Label(right_frame, text="Enter an Effect:").grid(row=0, column=0, padx=48, pady=10)
     effect_var = StringVar(right_frame)
     OptionMenu(right_frame, effect_var, *listOfEffects).grid(row=1, column=0, padx=10, pady=10)
     Button(right_frame, text="Search", command=lambda: search_effect(effect_var.get())).grid(row=2, column=0, padx=10, pady=10)
@@ -75,6 +96,11 @@ def open_effect_search():
 Button(left_frame, text="Search by Ingredient", command=open_ingredient_search,width=16,height=3,cursor="hand2").grid(row=0, column=0, pady=5)
 Button(left_frame, text="Search by Effect", command=open_effect_search,width=16,height=3,cursor="hand2").grid(row=1, column=0, pady=5)
 Button(left_frame, text="Quit", command=root.quit,width=16,height=3,cursor="hand2").grid(row=2, column=0, pady=5)
+Button(left_frame, text="Music On/Off", command=toggle_music ,width=12,height=2,cursor="hand2").grid(row=3, column=0, pady=20)
 
+
+play_background_music()
 
 root.mainloop()
+
+pygame.mixer.music.stop()
